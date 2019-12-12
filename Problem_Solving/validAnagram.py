2278d7bfce066b3964939d69b65625e392235549
Matthew Esqueda <matthewjw2007@yahoo.com>
2019-12-11 21:01:57 -0800

    Updated validAnagram

--- Problem_Solving/validAnagram.py
+++ Problem_Solving/validAnagram.py
@@ -1,4 +1,34 @@
 # This program checks to see if two words are an anagram of one another through the use of a frequency counter
 
+
+# Valid anagram function
+def valid_anagram(str_one, str_two):
+    # Create two dicts
+    dict_one = {}
+    dict_two = {}
+
+    # Verify the two inputs are the same size
+    if len(str_one) != len(str_two):
+        print("Different lengths, strings entered are not anagrams")
+        return False
+
+    # Transfer strings into the dicts
+
+    # Compare the dicts
+
+    # If all tests pass, return true
+    return True
+
+
 if __name__ == "__main__":
-    print("Hello World")
+    # Variables
+
+    # Get user input
+    print("Enter two words and I will let you know if they are an anagram")
+    input_one = input("First Word: ")
+    input_two = input("Second Word: ")
+
+    print("I will compare the following words: " + input_one + " and " + input_two)
+
+    # Call function
+    valid_anagram(input_one, input_two)
