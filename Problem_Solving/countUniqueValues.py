54c8261fa9fafc62e780354582dc6d0ba1e37e2b
Matthew Esqueda <matthewjw2007@yahoo.com>
2019-12-16 15:46:00 -0800

    Added countUniqueValues

---
+++ Problem_Solving/countUniqueValues.py
@@ -0,0 +1,4 @@
+# Matthew Esqueda
+# Program: Unique Values
+# Description: This program uses "pointers" to check how many unique values a list has and returns the total.
+# Last Update: December 16, 2019
